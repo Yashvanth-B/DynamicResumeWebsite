ffe56c67ca91b4b70fbb9c16a287f2c2e4182db4
Yashvanth-B <yashvanth.babu05@gmail.com>
2024-11-18 12:05:34 +0530

    ProjectUpdates

--- index.js
+++ index.js
@@ -6,82 +6,62 @@ var blueg = document.getElementById("bluebg");
 var mixedg = document.getElementById("mixed");
 var chtext = document.getElementById("hhh");
 
-
 function dark() {
-  darkmode.value = " darkmode-off ";
+  darkmode.value = "Normal Mode";
   side.style.color = "white";
-  side.style.backgroundImage=" linear-gradient(to right,rgba(0, 0, 0),rgb(0, 0, 0)) ";
-  mainscreen.style.backgroundImage =" linear-gradient(to right,rgba(0, 0, 0),rgb(0, 0, 0)) ";
+  side.style.backgroundImage =
+    " linear-gradient(to right,rgba(0, 0, 0),rgb(0, 0, 0)) ";
+  mainscreen.style.backgroundImage =
+    " linear-gradient(to right,rgba(0, 0, 0),rgb(0, 0, 0)) ";
 }
 
 function offd() {
-  darkmode.value = " darkmode-on  ";
+  darkmode.value = "Dark Mode ";
   side.style.backgroundImage =
     " linear-gradient(to right,rgb(124, 219, 187), rgb(48, 113, 100) )";
-  mainscreen.style.backgroundImage =" linear-gradient(to right,rgb(124, 219, 187), rgb(48, 113, 100) )";
-
+  mainscreen.style.backgroundImage =
+    " linear-gradient(to right,rgb(124, 219, 187), rgb(48, 113, 100) )";
 }
 
 function redon() {
-  side.style.backgroundImage ="linear-gradient(to right,rgb(24, 1, 97),rgb(79, 23, 135),rgb(235, 54, 120),rgb(251, 119, 60))";
-  mainscreen.style.backgroundImage = "linear-gradient(to right,rgb(24, 1, 97),rgb(79, 23, 135),rgb(235, 54, 120),rgb(251, 119, 60))";
+  side.style.backgroundImage =
+    "linear-gradient(to right,rgb(24, 1, 97),rgb(79, 23, 135),rgb(235, 54, 120),rgb(251, 119, 60))";
+  mainscreen.style.backgroundImage =
+    "linear-gradient(to right,rgb(24, 1, 97),rgb(79, 23, 135),rgb(235, 54, 120),rgb(251, 119, 60))";
 }
 function mixedon() {
-  side.style.backgroundImage ="linear-gradient(to right,rgb(0, 224, 172),rgb(13, 78, 62),rgb(67, 81, 75)) ";
-  mainscreen.style.backgroundImage ="linear-gradient(to right,rgb(0, 224, 172),rgb(13, 78, 62),rgb(67, 81, 75)) ";
+  side.style.backgroundImage =
+    "linear-gradient(to right,rgb(0, 224, 172),rgb(13, 78, 62),rgb(67, 81, 75)) ";
+  mainscreen.style.backgroundImage =
+    "linear-gradient(to right,rgb(0, 224, 172),rgb(13, 78, 62),rgb(67, 81, 75)) ";
 }
 function blueon() {
-  side.style.backgroundImage ="linear-gradient(to right,rgb(98, 35, 154),rgb(77, 77, 77),rgb(69, 53, 85)) ";
-  mainscreen.style.backgroundImage ="linear-gradient(to right,rgb(98, 35, 154),rgb(77, 77, 77),rgb(69, 53, 85))";
+  side.style.backgroundImage =
+    "linear-gradient(to right,rgb(98, 35, 154),rgb(77, 77, 77),rgb(69, 53, 85)) ";
+  mainscreen.style.backgroundImage =
+    "linear-gradient(to right,rgb(98, 35, 154),rgb(77, 77, 77),rgb(69, 53, 85))";
 }
 function offcc() {
   side.style.backgroundImage = "";
   side.style.color = "";
-  mainscreen.style.backgroundImage =" linear-gradient(to right,rgb(124, 219, 187), rgb(48, 113, 100) )";
+  mainscreen.style.backgroundImage =
+    " linear-gradient(to right,rgb(124, 219, 187), rgb(48, 113, 100) )";
 }
-function callhome()
-{
-  hhh.innerText="Home"
+function callhome() {
+  hhh.innerText = "Home";
 }
-function callaca() 
-{
+function callaca() {
   hhh.innerText = "Academics";
 }
 
-function callskills() 
-{
-    hhh.innerText = "Skills";
+function callskills() {
+  hhh.innerText = "Skills";
 }
 
-function callproject()
-{
-    hhh.innerText = "Project and Achievement";
+function callproject() {
+  hhh.innerText = "Project and Achievement";
 }
 
-function callabout()
-{
-    hhh.innerText = "About";
+function callabout() {
+  hhh.innerText = "About";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
